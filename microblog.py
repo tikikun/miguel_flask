e4518c35768da5e88bd07964c0e33d620cc92900
LAP11174 <lap11174@LAP11174s-MacBook-Pro.local>
2020-06-01 15:15:36 +0700

    changes in database

--- microblog.py
+++ microblog.py
@@ -1,4 +1,7 @@
-from app import app
+from app import app,db
+from app.models import User, Post
 
-#if __name__ == '__main__':
-#    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
